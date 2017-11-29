96ad2c6d83cfee326f062eef8faf885dcf5ff460
Takaki <p104314t@mail.kyutech.jp>
2017-11-29 16:04:16 +0000

    [change] file names

--- random_walk_dynamic_plot.py
+++ random_walk_dynamic_plot.py
@@ -3,6 +3,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
+import seaborn as sns
+sns.set_context('poster')
+
 
 def random_walker(start_position=0, mean=0, deviation=1, n_steps=99, seed=None):
 
@@ -29,36 +32,55 @@ def add_noise(position, mean=0, deviation=10, seed=None):
 
 if __name__ == '__main__':
 
-    start_position = 0.0
-    position = start_position
+    start_y = 0.0
 
-    fig, ax = plt.subplots(1, 1)
+    mean_y = 0.0
+    deviation_y = 1.0
+
+    mean_noize = 0.0
+    deviation_noize = 5
+
+    position = start_y
     x = np.arange(0, 1, 0.1)
-    y = np.random.normal(loc=0.0, scale=1.0, size=len(x))
+    move = np.random.normal(loc=mean_y, scale=deviation_y, size=len(x)-1)
+    y = np.insert(move, 0, start_y)
+    y = np.cumsum(y)
 
-    x = x.tolist()
-    y = y.tolist()
+    noise = np.random.normal(loc=mean_noize, scale=deviation_noize, size=len(y))
+    observed_y = y + noise
 
     print type(y)
-
     print len(x)
     print len(y)
-    lines, = ax.plot(x, y)
 
-    k = 0
+    fig, ax = plt.subplots(1, 1)
+    lines1, = ax.plot(x, y, 'r--', label='true y')
+    lines2, = ax.plot(x, observed_y, 'y', label='observed y')
+    plt.xlabel('time step')
+    plt.ylabel('y')
+    plt.legend(loc=1)
+    # plt.legend(loc='best')
+    plt.tight_layout()
+
     while True:
 
-        k+=1
-        move = np.random.normal(loc=0.0, scale=1.0)
-        x.append(x[-1]+0.1)
-        y.append(y[-1]+move)
+        prev_x = x[-1]
+        prev_y = y[-1]
+        prev_observed_y = observed_y[-1]
 
-        print len(x)
-        print len(y)
-        lines.set_data(x, y)
+        x = np.roll(x, -1)
+        y = np.roll(y, -1)
+        observed_y = np.roll(observed_y, -1)
 
-        ax.set_xlim((-0.1+0.1*k, x[-1]+0.1))
-        ax.set_ylim((-5, 5))
+        x[-1] = prev_x + 0.1
+        y[-1] = prev_y + np.random.normal(loc=mean_y, scale=deviation_y)
+        observed_y[-1] = y[-1] + np.random.normal(loc=mean_noize, scale=deviation_noize)
+
+        lines1.set_data(x, y)
+        lines2.set_data(x, observed_y)
+
+        ax.set_xlim((x.min(), x.max()))
+        ax.set_ylim((observed_y.min()-5, observed_y.max()+5))
 
         plt.pause(.05)
 
