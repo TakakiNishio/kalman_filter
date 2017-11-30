497817b49dfd6922c87c9496535a71d07c08298e
TakakiNishio <p104314t@mail.kyutech.jp>
2017-11-30 16:03:05 +0900

    [modify] optimized script

--- kalman_filter_basic_real_time2.py
+++ kalman_filter_basic_real_time2.py
@@ -2,43 +2,33 @@
 #-*-coding:utf-8-*-
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as anim
 import seaborn as sns
 sns.set_context('poster')
 
 
 class Kalman:
 
-    def __init__(self, observation, start_position, start_deviation, deviation_true, deviation_noise):
+    def __init__(self, start_position, start_deviation, deviation_position, deviation_noise):
 
-        self.obs = observation
-        self.n_obs = len(observation)
-        self.start_pos = start_position
-        self.start_dev = start_deviation
-        self.dev_q = deviation_true
-        self.dev_r = deviation_noise
+        self.prev_y = start_position
+        self.dev_q = deviation_position
+        self.dev_r = deviation_noize
+        self.prev_P = start_dev
+        self.start_flag = False
 
-        self.x_prev_ = [self.start_pos]
-        self.P_prev_ = [self.start_dev]
-        self.K_ = [self.P_prev_[0] / (self.P_prev_[0] + self.dev_r)]
-        self.P_ = [self.dev_r * self.P_prev_[0] / (self.P_prev_[0] + self.dev_r)]
-        self.x_ = [self.x_prev_[0] + self.K_[0] * (self.obs[0] - self.x_prev_[0])]
+    def forward(self, observation):
 
+        if self.start_flag is False:
+            self.start_flag = True
+        else:
+            self.prev_P = self.P_ + self.dev_q
 
-        self._forward()
+        K = self.prev_P / (self.prev_P + self.dev_r)
+        self.P_ = self.dev_r * self.prev_P / (self.prev_P + self.dev_r)
+        self.y_ = self.prev_y + K * (observation - self.prev_y)
+        self.prev_y = self.y_
 
-
-    def _forward(self):
-
-        for t in range(1, self.n_obs):
-
-            self.x_prev_.append(self.x_[t-1])
-            self.P_prev_.append(self.P_[t-1] + self.dev_q)
-
-            self.K_.append(self.P_prev_[t] / (self.P_prev_[t] + self.dev_r))
-            self.P_.append(self.dev_r * self.P_prev_[t] / (self.P_prev_[t] + self.dev_r))
-            self.x_.append(self.x_prev_[t] + self.K_[t] * (self.obs[t] - self.x_prev_[t]))
-
+        return self.y_
 
 
 if __name__ == '__main__':
@@ -53,9 +43,8 @@ if __name__ == '__main__':
     deviation_noize = 5
 
     position = start_y
-    x = np.arange(0, time_step*50, time_step)
-    print len(x)
-    move = np.random.normal(loc=mean_y, scale=deviation_y, size=len(x)-1)
+    t = np.arange(0, time_step*50, time_step)
+    move = np.random.normal(loc=mean_y, scale=deviation_y, size=len(t)-1)
     y = np.insert(move, 0, start_y)
     y = np.cumsum(y)
 
@@ -64,69 +53,55 @@ if __name__ == '__main__':
 
     # initialization
     obs = observed_y
+
+    start_obs = observed_y[0]
+
     n_obs = len(observed_y)
     start_pos = start_y
     start_dev = deviation_y
     dev_q = deviation_y
     dev_r = deviation_noize
 
-    x_prev_ = [start_pos]
-    P_prev_ = [start_dev]
-    K_ = [P_prev_[0] / (P_prev_[0] + dev_r)]
-    P_ = [dev_r * P_prev_[0] / (P_prev_[0] + dev_r)]
-    x_ = [x_prev_[0] + K_[0] * (obs[0] - x_prev_[0])]
+    kf = Kalman(start_pos, start_dev, dev_q, dev_r)
+    y_ = [kf.forward(start_obs)]
 
-    for t in range(1, n_obs):
+    for i in range(1, n_obs):
 
-        x_prev_.append(x_[t-1])
-        P_prev_.append(P_[t-1] + dev_q)
+        y_.append(kf.forward(obs[i]))
 
-        K_.append(P_prev_[t] / (P_prev_[t] + dev_r))
-        P_.append(dev_r * P_prev_[t] / (P_prev_[t] + dev_r))
-        x_.append(x_prev_[t] + K_[t] * (obs[t] - x_prev_[t]))
-
-    x_prev_ = np.array(x_prev_)
-    P_prev_ = np.array(P_prev_)
-    K_ = np.array(K_)
-    P_ = np.array(P_)
-    x_ = np.array(x_)
+    y_ = np.array(y_)
 
     fig, ax = plt.subplots(1, 1)
-    lines1, = ax.plot(x, y, 'r--', label='true y')
-    lines2, = ax.plot(x, observed_y, 'y', label='observed y')
-    lines3, = ax.plot(x, x_, 'blue' ,label='estimated y')
+    lines1, = ax.plot(t, y, 'r--', label='true y')
+    lines2, = ax.plot(t, observed_y, 'y', label='observed y')
+    lines3, = ax.plot(t, y_, 'blue' ,label='estimated y')
     plt.xlabel('time step')
     plt.ylabel('y')
     plt.legend(loc=1)
     plt.tight_layout()
 
-
     while True:
 
-        initial_y = y[0]
-        prev_x = x[-1]
         prev_y = y[-1]
-        prev_observed_y = observed_y[-1]
-        prev_P = P_[-1]+dev_q
+        prev_t = t[-1]
 
-        x = np.roll(x, -1)
+        t = np.roll(t, -1)
         y = np.roll(y, -1)
         observed_y = np.roll(observed_y, -1)
-        P_ = np.roll(P_, -1)
-        x_ = np.roll(x_, -1)
+        y_ = np.roll(y_, -1)
 
-        x[-1] = prev_x + time_step
-        y[-1] = prev_y + np.random.normal(loc=mean_y, scale=deviation_y)
-        observed_y[-1] = y[-1] + np.random.normal(loc=mean_noize, scale=deviation_noize)
+        t[-1] = prev_t + time_step
+        move = np.random.normal(loc=mean_y, scale=deviation_y)
+        y[-1] = prev_y + move
+        noise = np.random.normal(loc=mean_noize, scale=deviation_noize)
+        observed_y[-1] = y[-1] + noise
 
-        K = prev_P / (prev_P + dev_r)
-        P_[-1] = dev_r * prev_P / (prev_P + dev_r)
-        x_[-1] = prev_y + K * (observed_y[-1] - prev_y)
+        y_[-1] = kf.forward(observed_y[-1])
 
-        lines1.set_data(x, y)
-        lines2.set_data(x, observed_y)
-        lines3.set_data(x, x_)
-        ax.set_xlim((x.min(), x.max()))
+        lines1.set_data(t, y)
+        lines2.set_data(t, observed_y)
+        lines3.set_data(t, y_)
+        ax.set_xlim((t.min(), t.max()))
         ax.set_ylim((observed_y.min()-5, observed_y.max()+5))
 
         plt.pause(0.01)
